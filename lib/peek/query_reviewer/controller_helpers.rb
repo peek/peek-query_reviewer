96fdbe3b7518ada9a15532eaeefc41f9b76eaf42
dewski <me@garrettbjerkhoel.com>
2013-05-11 00:51:00 -0700

    Let the controller handle enabling and disabling query reviewer

---
+++ lib/peek/query_reviewer/controller_helpers.rb
@@ -0,0 +1,22 @@
+module Peek
+  module QueryReviewer
+    module ControllerHelpers
+      extend ActiveSupport::Concern
+
+      included do
+        before_filter :enable_query_reviewer, :if => :query_reviewer_enabled?
+        helper_method :query_reviewer_enabled?
+      end
+
+      protected
+
+      def enable_query_reviewer
+        Peek::QueryReviewer.enable!
+      end
+
+      def query_reviewer_enabled?
+        peek_enabled? && cookies[:query_review_enabled]
+      end
+    end
+  end
+end
